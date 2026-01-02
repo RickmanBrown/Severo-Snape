6eccc0f2a6d6f71c582f495061aed4986c6cb7c4
Paloma Brito <palomabarbosabrito.dev@gmail.com>
2026-01-01 22:54:35 -0300

    Projeto inicial com Vite e React

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="container">
+      <h1>O Diário do Príncipe Mestiço</h1>
+      <p>Projeto inicial em HTML e CSS</p>
+    </main>
+  );
+}
+
+export default App;
+
